8d8094a516574213ef3a195beea60786135ff0c8
fhrn71 <frans.harinck@harisoft.nl>
2017-11-07 20:49:30 +0100

    Added implementation of certificate and persistence of configuration and grants.

---
+++ Harisoft.IDP/Entities/ConfigurationDbContextExtensions.cs
@@ -0,0 +1,39 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using System.Linq;
+
+namespace Harisoft.IDP.Entities
+{
+    public static class ConfigurationDbContextExtensions
+    {
+        public static void EnsureSeedDataForContext(this ConfigurationDbContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                foreach (var client in Config.GetClients())
+                {
+                    context.Clients.Add(client.ToEntity());
+                }
+                context.SaveChanges();
+            }
+
+            if (!context.IdentityResources.Any())
+            {
+                foreach (var resource in Config.GetIdentityResources())
+                {
+                    context.IdentityResources.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+
+            if (!context.ApiResources.Any())
+            {
+                foreach (var resource in Config.GetApiResources())
+                {
+                    context.ApiResources.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Harisoft.IDP/Startup.cs
+++ Harisoft.IDP/Startup.cs
@@ -1,5 +1,4 @@
-﻿using Harisoft.IDP.Test;
-using IdentityServer4;
+﻿using IdentityServer4;
 using Harisoft.IDP.Entities;
 using Harisoft.IDP.Services;
 using Microsoft.AspNetCore.Builder;
@@ -8,12 +7,17 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Security.Cryptography.X509Certificates;
+using System;
+using System.Reflection;
+using IdentityServer4.EntityFramework.DbContexts;
 
 namespace Harisoft.IDP
 {
     public class Startup
     {
         public static IConfigurationRoot Configuration;
+
 
         public Startup(IHostingEnvironment env)
         {
@@ -29,39 +33,81 @@ namespace Harisoft.IDP
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
             var connectionString = Configuration["connectionStrings:marvinUserDBConnectionString"];
             services.AddDbContext<MarvinUserContext>(o => o.UseSqlServer(connectionString));
 
             services.AddScoped<IMarvinUserRepository, MarvinUserRepository>();
 
-            services.AddMvc();
+            var identityServerConnectionString = Configuration["connectionStrings:identityServerDataDBConnectionString"];
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
 
             services.AddIdentityServer()
-                .AddDeveloperSigningCredential()
+                .AddSigningCredential(LoadCertificateFromStore(Configuration["signingCredentialCertificateThumbPrint"]))
                 .AddMarvinUserStore()
-                .AddInMemoryIdentityResources(Config.GetIdentityResources())
-                .AddInMemoryApiResources(Config.GetApiResources())
-                .AddInMemoryClients(Config.GetClients());
-            services.AddAuthentication().AddFacebook("Facebook", "Facebook", options => {
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = (context) => context.UseSqlServer(identityServerConnectionString, sqlOptions =>
+                    {
+                        sqlOptions.MigrationsAssembly(migrationsAssembly);
+                    });
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = (context) => context.UseSqlServer(identityServerConnectionString, sqlOptions =>
+                     {
+                         sqlOptions.MigrationsAssembly(migrationsAssembly);
+                     });
+                });
+
+            services.AddAuthentication().AddFacebook("Facebook", "Facebook", options =>
+            {
                 options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
                 options.AppId = "UserYourOwnAppId";
                 options.AppSecret = "UseYourOwnSecret";
             }).AddCookie("idsrv.2FA");
         }
 
+        public X509Certificate2 LoadCertificateFromStore(string thumbPrint)
+        {
+            using (var store = new X509Store(StoreName.My, StoreLocation.LocalMachine))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                var certCollection = store.Certificates.Find(X509FindType.FindByThumbprint,
+                    thumbPrint, true);
+                if (certCollection.Count == 0)
+                {
+                    throw new Exception("The specified certificate wasn't found. Check the specified thumbprint.");
+                }
+                return certCollection[0];
+            }
+        }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, MarvinUserContext marvinUserContext)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory,
+            MarvinUserContext marvinUserContext, ConfigurationDbContext configurationDbContext,
+            PersistedGrantDbContext persistedGrantDbContext)
         {
             loggerFactory.AddConsole();
-            loggerFactory.AddDebug();
 
             if (env.IsDevelopment())
             {
+                loggerFactory.AddDebug();
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                persistedGrantDbContext.Database.Migrate();
 
-            marvinUserContext.Database.Migrate();
-            marvinUserContext.EnsureSeedDataForContext();
+                configurationDbContext.Database.Migrate();
+                configurationDbContext.EnsureSeedDataForContext();
+
+                marvinUserContext.Database.Migrate();
+                marvinUserContext.EnsureSeedDataForContext();
+            }
+
             app.UseIdentityServer();
             app.UseAuthentication();
             app.UseStaticFiles();
